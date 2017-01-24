086820647c13be9f663fa26284a94aa3b025a3c9
Motti <=>
2017-01-24 17:54:28 -0500

    added associations to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+
+	has_many :reservation_rooms
+	has_many :reservations, through: :reservation_rooms
+	has_many :rooms, through: :reservation_rooms
+
+
 	has_secure_password
 end
