a7a0f7911e4f7334d7317d18aee4369f8a566b81
Motti <=>
2017-01-24 17:27:02 -0500

    created columns

--- db/migrate/20170124221305_create_users.rb
+++ db/migrate/20170124221305_create_users.rb
@@ -1,8 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
-      t.timestamps
+    	t.string :username
+    	t.password :password_digest
+  		t.string :email
+    	t.string :first_name
+    	t.string :last_name
+    	t.string :street_address
+    	t.string :city
+    	t.string :zip 
     end
   end
 end
--- db/migrate/20170124221318_create_reservations.rb
+++ db/migrate/20170124221318_create_reservations.rb
@@ -1,8 +1,10 @@
 class CreateReservations < ActiveRecord::Migration[5.0]
   def change
     create_table :reservations do |t|
-
-      t.timestamps
+    	t.datetime :checkin
+    	t.datetime :checkout
+    	t.integer :room_number, default: nil
+    	t.integer :guests 
     end
   end
 end
